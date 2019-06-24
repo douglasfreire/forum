2ea21fadd47359c2efb7670b084099c3809dc27f
douglasfreire <douglasfreire20@gmail.com>
2019-06-24 15:43:22 -0300

    Aplicação de exmplo alura

---
+++ src/main/java/com/douglas/forum/model/StatusTopico.java
@@ -0,0 +1,10 @@
+package com.douglas.forum.model;
+
+public enum StatusTopico {
+	
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+
+}
---
+++ src/main/java/com/douglas/forum/repository/CursoRepository.java
@@ -0,0 +1,9 @@
+package com.douglas.forum.repository;
+
+import com.douglas.forum.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    Curso findByNome(String nome);
+}
